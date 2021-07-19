ed72c09e2a7296922744cdb0ea2ed46dcffda1b8
denbo12 <denbiprivate@gmail.com>
2021-07-19 17:51:08 +0100

    Initial commit

---
+++ app/src/main/java/com/denbofa/fragment/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.denbofa.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.denbofa.fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigate(AFragment())
+
+    }
+
+    fun navigate(fragment: Fragment){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragment_container_view, fragment)
+        }
+    }
+}
